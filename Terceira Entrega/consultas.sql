a78bce0b1e19ef7592e9ae8fdedb2a37259a2a93
= <=>
2019-06-29 21:27:30 -0300

    Bug fixes

--- Terceira Entrega/consultas.sql
+++ Terceira Entrega/consultas.sql
@@ -2,7 +2,7 @@ SELECT pac.nome as nome_responsavel, pac2.nome as nome_dependente
 FROM paciente pac
 INNER JOIN paciente pac2 ON pac.cpf = pac2.cpf_responsavel;
 
-SELECT pac.nome, pront.informacoes FROM prontuario
+SELECT pac.nome, pront.informacoes FROM prontuario pront
 INNER JOIN paciente pac ON pac.cpf = pront.cod_paciente
 WHERE paciente_tipo = 2;
 
