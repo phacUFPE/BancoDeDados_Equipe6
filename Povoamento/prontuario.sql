35d9a9c435bc98c44affbb10ba9df23b6c37210e
= <=>
2019-06-29 21:26:22 -0300

    Prontuario population

---
+++ Povoamento/prontuario.sql
@@ -0,0 +1,5 @@
+INSERT INTO prontuario(cod_paciente, informacoes) VALUES (94608573829, 'Paciente apresentava sintomas de dengue, foi feito o teste e retornou positivo, foram receitados os remedios e ele ficou em observação por 1 dia.');
+INSERT INTO prontuario(cod_paciente, informacoes) VALUES (09385792874, 'Paciente estava reclamando de nauseas e foi feito o teste de gravidez.');
+INSERT INTO prontuario(cod_paciente, informacoes) VALUES (47492012854, 'Paciente estava com diareia e vomito, foi ministrado soro para hidratação e antibioticos.');
+INSERT INTO prontuario(cod_paciente, informacoes) VALUES (78407583628, 'Paciente apresentava sintomas de virose, foram receitados remedios.');
+INSERT INTO prontuario(cod_paciente, informacoes) VALUES (35476585333, 'Paciente apresentava dedinho da mão esquerda inchado, foi feito raio-x e constatado que havia trincado.');
