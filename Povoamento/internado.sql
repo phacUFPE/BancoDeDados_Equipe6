e564f91d415056064f10767c997e6d2cb325d7f1
= <=>
2019-06-29 21:28:49 -0300

    Updated 1 date

--- Povoamento/internado.sql
+++ Povoamento/internado.sql
@@ -1,5 +1,3 @@
-INSERT INTO prontuario(cod_paciente, informacoes) VALUES (94608573829, 'Paciente apresentava sintomas de dengue, foi feito o teste e retornou positivo, foram receitados os remedios e ele ficou em observação por 1 dia.');
-INSERT INTO prontuario(cod_paciente, informacoes) VALUES (09385792874, 'Paciente estava reclamando de nauseas e foi feito o teste de gravidez.');
-INSERT INTO prontuario(cod_paciente, informacoes) VALUES (47492012854, 'Paciente estava com diareia e vomito, foi ministrado soro para hidratação e antibioticos.');
-INSERT INTO prontuario(cod_paciente, informacoes) VALUES (78407583628, 'Paciente apresentava sintomas de virose, foram receitados remedios.');
-INSERT INTO prontuario(cod_paciente, informacoes) VALUES (35476585333, 'Paciente apresentava dedinho da mão esquerda inchado, foi feito raio-x e constatado que havia trincado.');
+INSERT INTO internado(cod_paciente, cod_hospital, data_internacao) VALUES (99863759281, '43.765.066/0030-01', DATE '2016-09-25');
+INSERT INTO internado(cod_paciente, cod_hospital, data_internacao) VALUES (11176654943, '94.865.234/0003-65', DATE '2018-03-12');
+INSERT INTO internado(cod_paciente, cod_hospital, data_internacao) VALUES (78407583628, '05.914.964/0390-78', DATE '2017-01-17');
