566a79a73f8d64886ff8f988cd9359a27a2a2eb7
= <=>
2019-06-29 21:26:45 -0300

    Updated paciente population

--- Povoamento/paciente.sql
+++ Povoamento/paciente.sql
@@ -1,7 +1,7 @@
-INSERT INTO paciente(CPF, nome, data_nascimento, CPF_responsavel, paciente_tipo) VALUES (11176654943, 'JosÃ© Aldo Menezes', DATE '1975-03-04', null, 1);
-INSERT INTO paciente(CPF, nome, data_nascimento, CPF_responsavel, paciente_tipo) VALUES (12345678900, 'JosÃ© Aldo Filho', DATE '2005-02-03', 11176654943, 2);
+INSERT INTO paciente(CPF, nome, data_nascimento, CPF_responsavel, paciente_tipo) VALUES (11176654943, 'José Aldo Menezes', DATE '1975-03-04', null, 1);
+INSERT INTO paciente(CPF, nome, data_nascimento, CPF_responsavel, paciente_tipo) VALUES (12345678900, 'José Aldo Filho', DATE '2005-02-03', 11176654943, 2);
 INSERT INTO paciente(CPF, nome, data_nascimento, CPF_responsavel, paciente_tipo) VALUES (09385792874, 'Maria Emilia', DATE '1987-06-14', null, 1);
-INSERT INTO paciente(CPF, nome, data_nascimento, CPF_responsavel, paciente_tipo) VALUES (87598912944, 'Carlos JosÃ© Fernz', DATE '1956-01-30', null, 1);
+INSERT INTO paciente(CPF, nome, data_nascimento, CPF_responsavel, paciente_tipo) VALUES (87598912944, 'Carlos José Fernz', DATE '1956-01-30', null, 1);
 INSERT INTO paciente(CPF, nome, data_nascimento, CPF_responsavel, paciente_tipo) VALUES (35476585333, 'Juliana Gonzalez', DATE '1981-09-26', null, 1);
 INSERT INTO paciente(CPF, nome, data_nascimento, CPF_responsavel, paciente_tipo) VALUES (78407583628, 'Mario Bros', DATE '1990-11-01', null, 1);
 INSERT INTO paciente(CPF, nome, data_nascimento, CPF_responsavel, paciente_tipo) VALUES (48984391234, 'Emilia Limeira de Souza', DATE '1989-07-14', null, 1);
