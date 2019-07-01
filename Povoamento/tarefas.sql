6b98c72b5f9820a60f7e81d9c59ec940ece43afa
= <=>
2019-06-30 23:12:37 -0300

    Add tarefas population

---
+++ Povoamento/tarefas.sql
@@ -0,0 +1,16 @@
+INSERT INTO tarefas(cod_funcionario, tarefas) VALUES(13765840271, 'Fazer curativos.');
+INSERT INTO tarefas(cod_funcionario, tarefas) VALUES(13765840271, 'Fazer suturas.');
+INSERT INTO tarefas(cod_funcionario, tarefas) VALUES(13765840271, 'Cuidar dos pacientes idosos.');
+INSERT INTO tarefas(cod_funcionario, tarefas) VALUES(13765840271, 'Dar banho nos pacientes impossibilitados.');
+INSERT INTO tarefas(cod_funcionario, tarefas) VALUES(13765840271, 'Acompanhar pacientes sozinhos.');
+
+INSERT INTO tarefas(cod_funcionario, tarefas) VALUES(67830608774, 'Fazer curativos.');
+INSERT INTO tarefas(cod_funcionario, tarefas) VALUES(67830608774, 'Fazer suturas.');
+
+INSERT INTO tarefas(cod_funcionario, tarefas) VALUES(18495746372, 'Cuidar dos pacientes idosos.');
+
+INSERT INTO tarefas(cod_funcionario, tarefas) VALUES(97591038673, 'Dar banho nos pacientes impossibilitados.');
+INSERT INTO tarefas(cod_funcionario, tarefas) VALUES(97591038673, 'Acompanhar pacientes sozinhos.');
+INSERT INTO tarefas(cod_funcionario, tarefas) VALUES(97591038673, 'Fazer suturas.');
+
+INSERT INTO tarefas(cod_funcionario, tarefas) VALUES(13765840271, 'Tarefa que não deve ser adicionada.');
