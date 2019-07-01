784c49b97af09cf4a799a75b4cb27bbc27ce90dd
= <=>
2019-06-30 23:13:25 -0300

    Update consultas

--- Terceira Entrega/consultas.sql
+++ Terceira Entrega/consultas.sql
@@ -11,19 +11,21 @@ WHERE NOT EXISTS
 (SELECT * FROM internado inter
 WHERE pac.cpf = inter.cod_paciente);
 
-SELECT pac.nome as nome_paciente, func.nome as nome_funcionario
+SELECT pac.nome FROM paciente pac
+WHERE EXISTS
+(SELECT * FROM internado inter
+WHERE pac.cpf = inter.cod_paciente);
+
+SELECT pac.nome as nome_paciente, func.nome as nome_funcionario, cons.data_consulta
 FROM paciente pac
 INNER JOIN consulta cons ON cons.cod_paciente = pac.cpf
 INNER JOIN funcionario func ON cons.cod_funcionario = func.cpf
 WHERE func.cpf = 84758502975;
 
-SELECT * FROM ambulatorio
-WHERE NOT EXISTS
-(SELECT * FROM funcionario);
-
-SELECT func.nome, COUNT(trf.tarefas) FROM tarefas trf
-INNER JOIN funcionario func ON func.cpf = trf.cod_funcionario
-HAVING COUNT(trf.tarefas) >= 5;
+SELECT func.nome, COUNT(trf.tarefas) as tarefas FROM funcionario func
+INNER JOIN tarefas trf ON trf.cod_funcionario = func.cpf
+GROUP BY func.nome
+HAVING COUNT(trf.tarefas) = 5;
 
 SELECT * FROM hospital hosp
 INNER JOIN hospital_ambulatorio hamb ON hamb.cod_hospital = hosp.cnpj
